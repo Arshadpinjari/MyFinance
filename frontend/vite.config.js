1840bb5bae30eeef7de9dba9fb272bc2b314f157
Arshadpinjari <arshadpinjari7860@gmail.com>
2025-03-05 02:36:52 +0530

    Uploaded new project files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: "0.0.0.0", // Change localhost to 0.0.0.0 to listen on all network interfaces
     port: 8080,
     proxy: {
       "/api/v1": {
-        target: import.meta.env.VITE_BACKEND_API_URL || "http://localhost:3000",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
     },
-  },
-  build: {
-    outDir: "dist", // Ensure Vercel recognizes this directory
   },
 }));
